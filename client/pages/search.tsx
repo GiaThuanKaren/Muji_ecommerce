238e9b087cfbfbaa1b3e8169f897271c39ca55cb
Nguyễn Quang Gia Thuận <GiaThuanNguyen213@gmail.com>
2023-07-06 14:11:53 +0700

    New page - Search page

---
+++ client/pages/search.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { CardProduct } from 'src/Components'
+import { MainLayout } from 'src/Layouts'
+
+function SearchPage() {
+    return (
+        <>
+            <MainLayout>
+                <div className='flex flex-wrap'>
+                    {
+                        Array.from(Array(10).keys()).map(() => {
+                            return <>
+                                <CardProduct />
+                            </>
+                        })
+                    }
+                </div>
+            </MainLayout>
+        </>
+    )
+}
+
+export default SearchPage
