543561dbdb3f2fd729304a5762f71b4136599d05
Nguyễn Quang Gia Thuận <GiaThuanNguyen213@gmail.com>
2023-07-26 20:20:08 +0700

    feat - send mail with template html

--- server/src/main/java/com/muji_ecomerce/server/controller/RegistrationController.java
+++ server/src/main/java/com/muji_ecomerce/server/controller/RegistrationController.java
@@ -4,6 +4,7 @@ import com.muji_ecomerce.server.entity.Customer;
 import com.muji_ecomerce.server.model.CustomerModel;
 import com.muji_ecomerce.server.services.CustomerService;
 import com.muji_ecomerce.server.services.EmailService;
+import jakarta.mail.MessagingException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +22,7 @@ public class RegistrationController {
     private EmailService emailService;
 
     @PostMapping("/register")
-    public String registerNewCustomer(@RequestBody CustomerModel customerModel){
+    public String registerNewCustomer(@RequestBody CustomerModel customerModel) throws MessagingException {
         Customer customer = customerService.registerNewCCustomer(customerModel);
         emailService.sendMail("sguworkspace1@gmail.com","Hi Test Mail","Body Test");
         System.out.println("lksjdflds");
--- server/src/main/java/com/muji_ecomerce/server/services/EmailService.java
+++ server/src/main/java/com/muji_ecomerce/server/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.muji_ecomerce.server.services;
 
+import jakarta.mail.MessagingException;
+
 public interface EmailService {
-    void sendMail(String toEmail,String subjectEmail,String bodyEmail);
+    void sendMail(String toEmail,String subjectEmail,String bodyEmail) throws MessagingException;
 }
